f187fa5335d540c00bfcb77169b2841cfed243dd
dev-ricks <richard.smit.dev@gmail.com>
2026-03-29 22:53:54 -0400

    Integration tests created for the catalog-service.

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,47 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
+
+	private WebTestClient webTestClient;
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		this.webTestClient = WebTestClient.bindToServer()
+										  .baseUrl("http://localhost:" + port)
+										  .build();
+	}
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn())
+							.isEqualTo(expectedBook.isbn());
+				});
+	}
 }
